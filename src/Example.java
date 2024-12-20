0f31d3b232ed69383de7dc39df84b4662c9c01fa
sumancfc <sumanstha999@gmail.com>
2024-12-21 00:31:29 +0100

    Temperature conversion

--- src/Example.java
+++ src/Example.java
@@ -3,12 +3,19 @@ import java.util.Scanner;
 // Testing code Here
 public class Example {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter a int value: ");
+        // Scanner input = new Scanner(System.in);
+        // System.out.print("Enter a int value: ");
         // byte byteValue = input.nextByte();
-        int value = input.nextInt();
+        // int value = input.nextInt();
 
         // System.out.print("The given value is " + byteValue);
-        System.out.print("The given int value is " + value);
+        // System.out.print("The given int value is " + value);
+        // System.out.print("The reminder is " + 56 % 6);
+
+        // Today is Friday, what will be the day in 100 days?
+        // int dayInNumber = 2;
+        // int dayInHundredDays = (dayInNumber + 100) % 7;
+
+        // System.out.println("The day in 100 days will be: " + dayInHundredDays);
     }
 }
